782a06307d231c3fb2ee411ed10773ba1e64fb75
endabelyu <endabelyuproject@gmail.com>
2022-09-04 01:50:37 +0700

    add z-index to navbar

--- front-end/src/components/layout/Navbar.js
+++ front-end/src/components/layout/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = (props) => {
   // }
 
   return (
-    <nav className="fixed top-0 left-0 flex justify-between w-full bg-white h-25 py-3 px-8 md:px-6 drop-shadow-md  lg:p-4">
+    <nav className="fixed top-0 z-10 left-0 flex justify-between w-full bg-white h-25 py-3 px-8 md:px-6 drop-shadow-md  lg:p-4">
       <h1 className=" text-[#3371F2] text-4xl font-bold md:ml-2 lg:ml-14">
         fyp
       </h1>

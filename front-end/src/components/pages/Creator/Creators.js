fba3e6d4a007ca06a35f9c8742dd5183dd71ffb1
naswa13 <naswawilantama13@gmail.com>
2022-09-11 17:17:39 +0700

    convert creators page to react component

--- front-end/src/components/pages/Creator/Creators.js
+++ front-end/src/components/pages/Creator/Creators.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import Card from './card';
+import CreatorsData from './CreatorsData';
+import Image from '../../../assets/srcImg/srcImg';
 
 const Creators = () => {
+  const data = CreatorsData;
+  // console.log(data);
+
   return (
-    <div class="container mx-auto m-4">
-      <div>
-        <h1 class="title text-3xl text-center font-bold m-7">
-          This is our <br />
-          Incredible <p class="creators">Creators</p>
+    <React.Fragment>
+      <div className="mt-32 ">
+        <h1 class="title max-w-xl mb-10 text-3xl text-center font-bold  mx-auto">
+          This is our Incredible
+          <span class="creators ml-3 stroke text-white">Creators</span>
         </h1>
+        <div className="mx-auto flex gap-y-10 flex-wrap w-[90%] bg-white drop-shadow-xl p-10">
+          {data.map((creator) => {
+            const { name, image, quote } = creator;
+
+            return <Card name={name} image={image} quotes={quote} />;
+          })}
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
--- front-end/src/components/pages/Creator/card.js
+++ front-end/src/components/pages/Creator/card.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
 
-export const card = () => {
+const Card = (props) => {
+  const { name, image, quote } = props;
   return (
-    <div class="grid gap-4 grid-cols-2">
-      <div class="card basis-1/2 rounded-lg bg-[#EAEAFA] flex flex-wrap shadow md:shadow-lg">
-        <div class="card-foto">
+    <React.Fragment>
+      <div class="card flex flex-col w-80 mx-auto rounded-lg bg-[#EAEAFA] px-6">
+        <div class="profileCreator flex py-4 gap-x-4">
           <img
-            src="images/people.png"
-            alt=""
-            // style="border: 3px solid white; border-radius: 50%;"
+            src={image}
+            alt={name}
+            className=" w-20 border-3 border-white rounded-full"
           />
+          <div class="card-text mx-auto">
+            <p class="card-name mb-3">{name}</p>
+
+            <p class="card-quotes  text-sm italic">{quote}</p>
+          </div>
         </div>
+        <ul className="socmed flex justify-between p-4">
+          <NavLink to="">
+            <BsTwitter />
+          </NavLink>
+          <NavLink to="">
+            <BsInstagram />
+          </NavLink>
+          <NavLink to="">
+            <BsGithub />
+          </NavLink>
+        </ul>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
+
+export default Card;
